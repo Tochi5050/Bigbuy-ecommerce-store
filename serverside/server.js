37aba5565a254dd08efd2b0ffcad09a81f7924cd
Tochi5050 <ctochi46@gmail.com>
2023-06-13 09:07:40 +0100

    Latest changes to the root directory as at 13th of june 2023

--- serverside/server.js
+++ serverside/server.js
@@ -1,12 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from '../serverside/dbconnect/db.js'
 dotenv.config()
+
 const port = process.env.PORT
 
+connectDB() 
+
 const app = express()
+
+
 
 app.get('/', (req, res) => {
     res.send('running...')
 })
+
+
 
 app.listen(port)
